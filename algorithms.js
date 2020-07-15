ce9892d20517dfeff02a151ba03995a9f854e962
veecee <valentine424@yahoo.com>
2020-07-15 11:56:48 +0100

    roman numerals converter

--- algorithms.js
+++ algorithms.js
@@ -17,6 +17,38 @@ function palindrome(str) {
   
   palindrome("_eye");
 
+//Roman numeral converter
+var convertToRoman = function(num) {
+    var decimalNum = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romanNumerals = [
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I"
+    ];
+  
+    let romanNumeral = "";
+    for (let i = 0; i < decimalNum.length; i++) {
+        while (decimalNum[i] < num) {
+            romanNumeral+= romanNumerals[i]
+            num -= decimalNum[i]
+        }
+    }
+    return romanNumeral;
+  };
+  
+
+ convertToRoman(48);
+
 
 app.listen(2020, () => {
     console.log('Listening to everything!')
