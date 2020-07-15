092e1eaa5624047630c14af1e242fea36488a27e
veecee <valentine424@yahoo.com>
2020-07-15 07:34:03 +0100

    palindrome checker done

---
+++ algorithms.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+
+//Palindrome checker
+function palindrome(str) {
+    let filteredStr = str.toLowerCase().match(/[a-z0-9]/ig)
+    const newStr = filteredStr.join("")
+    const newStrRvs = filteredStr.reverse().join("")
+
+    if(newStr === newStrRvs) {
+        return true
+    }
+    return false
+  }
+  
+  
+  
+  palindrome("_eye");
+
+
+
+app.listen(2020, () => {
+    console.log('Listening to everything!')
+})
