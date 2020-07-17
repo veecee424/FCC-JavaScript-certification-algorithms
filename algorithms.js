1ef70490edd2e61d8fef1a267c64fef160ab1e74
veecee <valentine424@yahoo.com>
2020-07-17 20:33:32 +0100

    telephone validator done with lots of if statements

--- algorithms.js
+++ algorithms.js
@@ -65,7 +65,46 @@ var convertToRoman = function(num) {
     });
     return cipher.join("")
   }
-  console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
+
+
+  //Telephone validator
+function telephoneCheck(str) {
+    let numbers = str.match(/\d/g)
+    let symbols = str.match(/\W/g)
+    let acceptedSyms = ["-", ")", "(", " "]
+    if (numbers.length > 11 || numbers.length < 10) {
+        return false
+    }
+    if(str.charAt(0) == "-") {
+        return false
+    }
+    if(str.charAt(0) == "(" && str.charAt(str.length - 1) == ")") {
+        return false
+    }
+    if (numbers.length == 11 && numbers[0] != 1) {
+        return false
+    }
+
+    if (symbols) {
+        if (symbols.includes("(") && !symbols.includes(")")) {
+            return false
+        }
+        if (symbols.includes(")") && !symbols.includes("(")) {
+            return false
+        }
+        for (let i = 0; i < symbols.length; i++) {
+            if (acceptedSyms.indexOf(symbols[i]) == -1) {
+                return false
+            }
+        }
+        return true
+    }
+    return true
+  }
+  
+  console.log(telephoneCheck("(555-555-5555"));
+  
 
 
 app.listen(2020, () => {
